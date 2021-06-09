aa06639d71fe9bdce24b8a714c7b9614bb7c767e
Carolina Gilabert <carolgilabert@gmail.com>
2021-06-09 19:19:11 +0100

    Tweking message wording.

--- functions/wit-channel-reminder.js
+++ functions/wit-channel-reminder.js
@@ -17,7 +17,7 @@ async function postMessageToSlack(message) {
 
 exports.handler = async (event, context) => {
     try {
-        const message = ':mega:  PSA: We have a private WiT channel :wit-notts: \n\nIf you would like to join our safe space channel for women and non-binary, please see: https://nott.tech/2SQc3oR :orange_heart: ';
+        const message = ':mega:  PSA: We have a private WiT channel :wit-notts: \n\nIf you would like to join our safe space channel for women and non-binary folks, please see: https://nott.tech/2SQc3oR :orange_heart: ';
 
         await postMessageToSlack(message);
 
